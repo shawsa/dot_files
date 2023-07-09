754ed93fd392d7f83301e4f2b721cf16676518bc
sage <sage.b.shaw@gmail.com>
2023-07-09 17:29:07 -0600

    added the ipython vim-profile and set bash/ipython to use vi keybindings

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 SRC=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 #FILE=.bashrc
-for FILE in .bashrc .toprc .vimrc
+
+ipython profile create vim
+
+for FILE in .bashrc .toprc .vimrc .ipython/profile_vim/ipython_config.py .ipython/profile_vim/startup/imports.py
 do
 	if [[ -f ~/$FILE && ! -L ~/$FILE ]]; then
 		echo "$FILE found, creating backup ~/$FILE.backup"
