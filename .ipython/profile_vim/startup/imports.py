6074bd8d5e336568fcce5800e50cab689b808f27
Sage Shaw <sage.b.shaw@gmail.com>
2023-11-06 14:53:05 -0700

    autoreload magic loads automatically

--- .ipython/profile_vim/startup/imports.py
+++ .ipython/profile_vim/startup/imports.py
@@ -1,3 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 plt.ion()
+
+import IPython
+ipython = IPython.get_ipython()
+ipython.run_line_magic(magic_name="load_ext", line="autoreload")
+ipython.run_line_magic(magic_name="autoreload", line="2")
